25a7fb803fdd508e38d2dd8283078d112a16ca47
indu <indu.gadige@gmail.com>
2020-10-11 18:00:57 -0400

    added new text

--- src/Alert.js
+++ src/Alert.js
@@ -19,11 +19,9 @@ class AlertEx extends React.Component {
     if (show) {
       return (
         <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <Alert.Heading>If you are interested in these products ,please contact so and so</Alert.Heading>
           <p>
-            Change this and that and try again. Duis mollis, est non commodo
-            luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-            Cras mattis consectetur purus sit amet fermentum.
+            Contact details..............................
           </p>
         </Alert>
       );
